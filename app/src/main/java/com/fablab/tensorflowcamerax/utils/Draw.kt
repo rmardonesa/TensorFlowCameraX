780a26fc289f5442038b16b8e02354b292b738fd
RodrigoMardonesAlvarez <rodrigomardones@protonmail.com>
2023-01-26 23:54:02 -0300

    Supressing custom view constructor

--- app/src/main/java/com/fablab/tensorflowcamerax/utils/Draw.kt
+++ app/src/main/java/com/fablab/tensorflowcamerax/utils/Draw.kt
@@ -1,5 +1,6 @@
 package com.fablab.tensorflowcamerax.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -7,7 +8,9 @@ import android.graphics.Paint
 import android.graphics.Rect
 import android.view.View
 
+@SuppressLint("ViewConstructor")
 class Draw(context: Context?, var rect: Rect, var text: String): View(context) {
+
 
     lateinit var boundaryPaint: Paint
     lateinit var textPaint: Paint
